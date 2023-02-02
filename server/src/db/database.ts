b4d12d65dca1759bbcf47caae5188841c160bc32
Daniel Sales <danisales.es@gmail.com>
2023-02-02 15:51:09 +0100

    fix(back): compatible con nueva versión de mongoose

--- server/src/db/database.ts
+++ server/src/db/database.ts
@@ -1,66 +1,63 @@
 import dotenv from "dotenv";
-import mongoose from "mongoose";
+import mongoose, { ConnectOptions } from "mongoose";
+
 dotenv.config();
 
 // mongoose options
-const options = {
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-    autoIndex: false,
-    poolSize: 10,
-    bufferMaxEntries: 0
+const options: ConnectOptions = {
+  autoIndex: false,
+  maxPoolSize: 10,
 };
-
 // mongodb environment variables
-const {
-    MONGO_HOSTNAME,
-    MONGO_DB,
-    MONGO_PORT,
-    MONGO_USER,
-    MONGO_PASSWORD
-} = process.env;
-
+const {MONGO_HOSTNAME,
+  MONGO_DB,
+  MONGO_PORT,
+  MONGO_USER,
+  MONGO_PASSWORD} = process.env;
 const dbConnectionURL = generateUrl();
 
 function generateUrl() {
-    let ret = "mongodb+srv://";
-    if (MONGO_USER && MONGO_PASSWORD) {
-        ret += MONGO_USER + ":" + MONGO_PASSWORD + "@";
-    }
+  let ret = "mongodb+srv://";
 
-    ret += MONGO_HOSTNAME;
-    if (MONGO_PORT)
-        ret += ":" + MONGO_PORT;
+  if (MONGO_USER && MONGO_PASSWORD)
+    ret += `${MONGO_USER }:${ MONGO_PASSWORD }@`;
 
-    ret += "/" + MONGO_DB;
+  ret += MONGO_HOSTNAME;
 
-    return ret;
+  if (MONGO_PORT)
+    ret += `:${ MONGO_PORT}`;
+
+  ret += `/${ MONGO_DB}`;
+
+  return ret;
 }
 
 function connect() {
-    console.log(`Connecting to ${dbConnectionURL}...`);
-    mongoose.connect(dbConnectionURL, options);
-    const db = mongoose.connection;
-    db.on('error', console.error.bind(console, 'Mongodb Connection Error:' + dbConnectionURL));
-    db.once('open', () => {
-        // we're connected !
-        console.log('Mongodb Connection Successful!');
-    });
-    db.once('close', () => {
-        console.log('Mongodb Connection Closed!');
-    });
+  console.log(`Connecting to ${dbConnectionURL}...`);
+  mongoose.set("strictQuery", false);
+  mongoose.connect(dbConnectionURL, options);
+  const db = mongoose.connection;
+
+  db.on("error", console.error.bind(console, `Mongodb Connection Error:${ dbConnectionURL}`));
+  db.once("open", () => {
+    // we're connected !
+    console.log("Mongodb Connection Successful!");
+  } );
+  db.once("close", () => {
+    console.log("Mongodb Connection Closed!");
+  } );
 }
 
 function disconnect() {
-    mongoose.disconnect();
+  mongoose.disconnect();
 }
 
-async function connection(f: () => void) {
-    connect();
-    await f();
-    disconnect();
+async function connection(f: ()=> void) {
+  connect();
+  await f();
+  disconnect();
 }
 
-export { mongoose, connect, disconnect, connection };
+export {
+  mongoose, connect, disconnect, connection,
+};
