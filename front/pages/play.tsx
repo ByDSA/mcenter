eeff03e5b42431b7dfa8662a09511d59574106b7
Daniel Sales <danisales.es@gmail.com>
2023-09-09 23:45:31 +0200

    feat: añadido página y links de play stream

---
+++ front/pages/play.tsx
@@ -0,0 +1,55 @@
+import Head from "next/head";
+import { Fragment, MouseEventHandler } from "react";
+import styles from "./History.module.css";
+
+export default function Play() {
+  const playStream: (stream: string)=> MouseEventHandler = (stream: string) => (e) => {
+    e.preventDefault();
+    fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/play/stream/${stream}`, {
+      method: "GET",
+    } );
+  };
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>MCenter</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Play
+        </h1>
+
+        <h2>Streams</h2>
+        {
+          ["simpsons", "fguy", "futurama", "rick-morty"].map((stream) => (
+            <Fragment key={stream}>
+              <a onClick={playStream(stream)}>Play {stream}</a>
+              <br/>
+              <br/>
+            </Fragment>
+          ))
+        }
+      </main>
+      <style jsx>{`
+
+        a {
+          color: #0070f3;
+          text-decoration: none;
+        }
+
+        a:hover,
+        a:focus,
+        a:active {
+          text-decoration: underline;
+        }
+        a:hover {
+          color: #00f;
+          cursor: pointer;
+        }
+      `}</style>
+    </div>
+  );
+}
