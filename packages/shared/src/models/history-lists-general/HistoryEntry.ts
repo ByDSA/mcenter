44cbb54ec1563abaa1a504fe9f24643e99d303e6
Daniel Sales <danisales.es@gmail.com>
2024-01-09 12:20:19 +0100

    resource opcional en entrada de historial

--- packages/shared/src/models/history-lists-general/HistoryEntry.ts
+++ packages/shared/src/models/history-lists-general/HistoryEntry.ts
@@ -2,9 +2,10 @@
 import { z } from "zod";
 import { DateTypeSchema } from "../../utils/time";
 
-export function makeEntrySchema<T extends z.ZodTypeAny>(resourceId: T) {
+export function makeEntrySchema<T extends z.ZodTypeAny, U extends z.ZodTypeAny>(resourceId: T, resource: U) {
   return z.object( {
     resourceId,
+    resource: resource.optional(),
     date: DateTypeSchema,
   } ).strict();
 }
