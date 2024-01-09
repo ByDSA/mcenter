cea5da763eb64e3f265862f49cdb516ed0abd633
Daniel Sales <danisales.es@gmail.com>
2024-01-09 12:20:19 +0100

    resource opcional en entrada de historial

--- packages/shared/src/models/musics/history/Entry.ts
+++ packages/shared/src/models/musics/history/Entry.ts
@@ -3,8 +3,9 @@ import { getDateNow } from "../../../utils/time";
 import { assertZodPopStack } from "../../../utils/validation/zod";
 import { makeHistoryEntrySchema } from "../../history-lists-general";
 import { IDSchema, Id } from "../Entity";
+import { VOSchema } from "../VO";
 
-export const EntrySchema = makeHistoryEntrySchema(IDSchema);
+export const EntrySchema = makeHistoryEntrySchema(IDSchema, VOSchema);
 
 type Entry = z.infer<typeof EntrySchema>;
 export default Entry;
