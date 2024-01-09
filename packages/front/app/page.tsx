09995d23ad1625acbf30d562bd5f5b701c690c84
Daniel Sales <danisales.es@gmail.com>
2024-01-09 11:57:36 +0100

    mostrar la versión de mcenter

--- packages/front/app/page.tsx
+++ packages/front/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { version } from "../../package.json" assert { type: "json" };
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
@@ -15,6 +16,9 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://mcenter.danisales.es">MCenter!</a>
         </h1>
+        <h2 style={{
+          textAlign: "center",
+        }}>Versión: {version}</h2>
       </main>
 
     </div>
