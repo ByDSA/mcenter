355372ed1bf5876863482130e3bd6082df2b47f6
Daniel Sales <danisales.es@gmail.com>
2024-01-10 17:09:30 +0100

    backend api rest

--- packages/server/tests/main/utils.ts
+++ packages/server/tests/main/utils.ts
@@ -5,9 +5,9 @@ import { InjectionToken, container } from "tsyringe";
 /* eslint-disable import/prefer-default-export */
 export const getRouterMock: ()=> express.Router = jest.fn(() => express.Router());
 
-export function registerSingletonIfNotAndGet<T>(original: InjectionToken<T>, mock?: InjectionToken<PublicMethodsOf<T>>): T {
+export function registerSingletonIfNotAndGet<T, U extends PublicMethodsOf<T>>(original: InjectionToken<T>, mock?: InjectionToken<U>): U {
   if (!container.isRegistered(original))
     container.registerSingleton<PublicMethodsOf<T>>(original, mock ?? original);
 
-  return container.resolve(original);
+  return container.resolve(original) as unknown as U;
 }
