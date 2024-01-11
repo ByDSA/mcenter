1062d3ffee04807a56363d83ff56588bbeb05a12
Daniel Sales <danisales.es@gmail.com>
2024-01-11 13:16:55 +0100

    front funcional

--- packages/front/modules/music/history/HistoryList.tsx
+++ packages/front/modules/music/history/HistoryList.tsx
@@ -1,72 +1,12 @@
 
 /* eslint-disable require-await */
-import { assertIsHistoryListGetManyEntriesBySearchResponse } from "#shared/models/historyLists";
+import { FetchingRender } from "#modules/fetching";
 import { HistoryMusicEntry } from "#shared/models/musics";
-import Loading from "app/loading";
 import extend from "just-extend";
-import React, { Fragment } from "react";
+import { Fragment } from "react";
 import HistoryEntryElement from "./entry/HistoryEntry";
+import { useRequest } from "./request";
 import { getDateStr } from "./utils";
-
-let error;
-
-type T = HistoryMusicEntry;
-const data: Required<T>[] = [
-  {
-    date: {
-      timestamp: 1704900141,
-      day: 10,
-      month: 1,
-      year: 2024,
-    },
-    resourceId: "resourceId",
-    resource: {
-      title: "title",
-      weight: 0,
-      artist: "artist",
-      hash: "hash",
-      mediaInfo: {
-        duration: 0,
-      },
-      path: "path",
-      size: 0,
-      timestamps: {
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      url: "url",
-      tags: ["car", "work"],
-    },
-  },
-  {
-    date: {
-      timestamp: 1704799141,
-      day: 9,
-      month: 1,
-      year: 2024,
-    },
-    resourceId: "resourceId",
-    resource: {
-      title: "title",
-      weight: 0,
-      artist: "artist",
-      hash: "hash",
-      mediaInfo: {
-        duration: 2,
-      },
-      path: "path",
-      size: 123,
-      timestamps: {
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      url: "url",
-      tags: ["car", "work2"],
-    },
-  },
-];
-const isLoading = false;
-const URL = "";
 
 type Props = {
   splitByDay?: boolean;
@@ -77,42 +17,28 @@ const DEFAULT_PARAMS: Required<Props> = {
 
 export default function HistoryList(props?: Props) {
   const params = extend(true, DEFAULT_PARAMS, props) as typeof DEFAULT_PARAMS;
-  const [list, setList] = React.useState(data);
 
-  if (error)
-  {return <>
-    <p>Failed to load.</p>
-    <p>{URL}</p>
-    <p>{JSON.stringify(error, null, 2)}</p>
-  </>;}
-
-  if (!list && isLoading)
-    return <Loading/>;
-
-  if (list === undefined) {
-    setList(data);
-
-    assertIsHistoryListGetManyEntriesBySearchResponse(data);
-  }
-
-  return (
-    <span style={{
-      flexDirection:"column",
-      display:"flex",
-      alignItems:"center",
-      justifyContent:"center",
-    }}>
-      {
-        list && list.map((entry: Required<T>, i: number, array: Required<T>[]) => <Fragment key={`${entry.resourceId} ${entry.date.timestamp}`}>
-          {params.splitByDay ? dayTitle(entry, i, array) : null}
-          <HistoryEntryElement value={entry} />
-        </Fragment>)
-      }
-    </span>
-  );
+  return FetchingRender<Required<HistoryMusicEntry>[]>( {
+    useRequest,
+    render: (data) => (
+      <span style={{
+        flexDirection:"column",
+        display:"flex",
+        alignItems:"center",
+        justifyContent:"center",
+      }}>
+        {
+          data && data.map((entry, i, array) => <Fragment key={`${entry.resourceId} ${entry.date.timestamp}`}>
+            {params.splitByDay ? dayTitle(entry, i, array) : null}
+            <HistoryEntryElement value={entry} />
+          </Fragment>)
+        }
+      </span>
+    ),
+  } );
 }
 
-function dayTitle(entry: Required<T>, i: number, array: Required<T>[]) {
+function dayTitle(entry: Required<HistoryMusicEntry>, i: number, array: Required<HistoryMusicEntry>[]) {
   if (i === 0 || !isSameday(array[i - 1].date.timestamp, entry.date.timestamp))
     return <h2 key={getDateStr(new Date(entry.date.timestamp * 1000))}>{getDateStr(new Date(entry.date.timestamp * 1000))}</h2>;
 
---
+++ packages/front/modules/music/history/request.ts
@@ -0,0 +1,31 @@
+/* eslint-disable import/prefer-default-export */
+import { makeFetcher, makeUseRequest } from "#modules/fetching";
+import { getBackendUrl } from "#modules/utils";
+import { HistoryMusicEntry, HistoryMusicListGetManyEntriesBySearchRequest, assertIsHistoryMusicListGetManyEntriesBySearchResponse, assertIsMusicVO } from "#shared/models/musics";
+
+const body: HistoryMusicListGetManyEntriesBySearchRequest["body"] = {
+  "filter": {
+  },
+  "sort": {
+    "timestamp": "desc",
+  },
+  "limit": 10,
+  "expand": ["musics"],
+};
+const validator = (data: Required<HistoryMusicEntry>[]) => {
+  assertIsHistoryMusicListGetManyEntriesBySearchResponse(data);
+
+  for (const d of data)
+    assertIsMusicVO(d.resource);
+};
+const fetcher = makeFetcher( {
+  method: "POST",
+  body,
+  validator,
+} );
+
+export const useRequest = makeUseRequest<Required<HistoryMusicEntry>[]>( {
+  url: `${getBackendUrl()}/api/musics/history/user/search`,
+  fetcher,
+  refreshInterval: 1 * 1000,
+} );
