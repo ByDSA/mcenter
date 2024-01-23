838409418994f4ecaaea64e691d28a989f73bceb
Daniel Sales <danisales.es@gmail.com>
2024-01-23 02:31:40 +0100

    fix(front): update title styling and add new color variables

--- packages/front/app/page.tsx
+++ packages/front/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     <div className={`extra-margin ${styles.container}`}>
       <main>
         <h1 className={styles.title}>
-          Welcome to<br/><a href="https://mcenter.danisales.es">MCenter</a>!
+          <span className={styles.titlePart1}>Welcome to</span><br/><a href="https://mcenter.danisales.es">MCenter</a><span className={styles.titlePart2}>!!!</span>
         </h1>
         <h2 className={styles.version}>Versión: {version}</h2>
       </main>
