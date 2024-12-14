6a2d94be158d8318a027e0aa2b030492a90dd34d
Daniel Sales <danisales.es@gmail.com>
2024-12-14 23:33:46 +0100

    fix(back): chevrotain docker

--- packages/server/lib/install_chevrotain.sh
+++ packages/server/lib/install_chevrotain.sh
@@ -1,29 +1,43 @@
-#!/bin/bash
+#!/bin/sh
+set -e
 
 # Instala la dependencia "chevrotain" para que sea compatible con CommonJS
 
-SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
+echo "Carpeta del script: $SCRIPT_DIR"
 
 TEMP_DIR=$(mktemp -d)
 trap 'rm -rf "$TEMP_DIR"' EXIT
 cd "$TEMP_DIR"
 
-git clone https://github.com/Chevrotain/chevrotain
+echo "Clonando repositorio desde git ..."
+git clone https://github.com/Chevrotain/chevrotain  > /dev/null
 cd chevrotain
 
+echo "Instalando dependencias ..."
 pnpm i
+
+echo "Compilando ..."
 pnpm compile
 
+echo "Modificando package.json ..."
 cd packages/chevrotain
 sed -i '/"bundle": "npm-run-all bundle:\*\*",/a \ \ \ \ "bundle:cjs:regular": "esbuild ./lib/src/api.js --bundle --sourcemap --format=cjs --outfile=lib/chevrotain.cjs",\n    "bundle:cjs:min": "esbuild ./lib/src/api.js --bundle --minify --format=cjs --sourcemap --outfile=lib/chevrotain.min.cjs",' package.json
 
-pnpm bundle:cjs:regular
-pnpm bundle:cjs:min
+echo "Generando bundles ..."
+pnpm bundle:cjs:regular  > /dev/null
+pnpm bundle:cjs:min  > /dev/null
 
+echo "Modificando package.json con los nuevos bundles ..."
 sed -i '/"import": ".\/lib\/src\/api.js",/a \ \ \ \ \ \ "require": ".\/lib\/chevrotain.cjs",' package.json
 
+echo "Eliminando archivos no necesarios ..."
 rm -rf benchmark_web node_modules scripts test tsconfig.json .c8rc.json .mocharc.cjs
 
 cd $SCRIPT_DIR
 rm -rf chevrotain
+
+echo "Moviendo carpeta chevrotain para que pueda ser usada ..."
 mv $TEMP_DIR/chevrotain/packages/chevrotain ./
+
+echo "Fin!"
