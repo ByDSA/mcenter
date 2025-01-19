bd1ad8586e09abda075768a3b39df78d39916967
Daniel Sales <danisales.es@gmail.com>
2025-01-19 17:42:55 +0100

    fix(back): fix delete method

--- packages/server/src/modules/musics/repositories/Repository.ts
+++ packages/server/src/modules/musics/repositories/Repository.ts
@@ -218,7 +218,7 @@ CanGetOneById<Music, MusicID>
   }
 
   async deleteOneByPath(relativePath: string) {
-    const docOdm = await ModelOdm.findByIdAndDelete( {
+    const docOdm = await ModelOdm.findOneAndDelete( {
       path: relativePath,
     } );
 
