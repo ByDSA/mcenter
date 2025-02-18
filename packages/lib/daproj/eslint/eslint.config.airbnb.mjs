fa38a5948aeeb10fc861548ad6873d3da3a1bd3f
Daniel Sales <danisales.es@gmail.com>
2025-02-18 14:04:11 +0100

    chore: use daproj from external library

--- packages/lib/daproj/eslint/eslint.config.airbnb.mjs
+++
@@ -1,6 +0,0 @@
-
-import pluginAirbnb from "eslint-config-airbnb-base/rules/style";
-
-export const { rules } = pluginAirbnb;
-
-export const plugin = pluginAirbnb;
--- packages/lib/daproj/index.mjs
+++
@@ -1,8 +0,0 @@
-export const Dependencies = {
-  TypeScript: "ts",
-  Jest: "jest",
-  Prettier: "prettier",
-  Eslint: "eslint",
-  React: "react",
-  Node: "node",
-};
