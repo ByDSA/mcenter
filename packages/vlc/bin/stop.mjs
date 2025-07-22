c5782653b5b76f05407f376e434078e0ab0713d6
Daniel Sales <danisales.es@gmail.com>
2025-07-22 02:20:29 +0200

    chore(vlc): scripts

--- packages/vlc/bin/stop.mjs
+++ packages/vlc/bin/stop.mjs
@@ -1,18 +1,16 @@
 #!/usr/bin/env zx
 $.verbose = true;
 
-const packageJson = await fs.readJson("../package.json");
+const packageJson = await fs.readJson("./package.json");
 const startScript = packageJson.scripts.start;
+const result = await $`ps -eo pid,cmd | grep ${startScript} | grep -v grep |
+  awk '{print $1}'`.nothrow();
+const pid = (result.stdout ?? "").trim();
 
-try {
-  const pid = await $`ps -eo pid,cmd | grep ${startScript} | grep -v grep | awk '{print $1}'`;
+if (pid) {
+  const pids = pid.stdout.trim().replace(/\n/g, " ");
 
-  if (pid.stdout.trim()) {
-    const pids = pid.stdout.trim().replace(/\n/g, " ");
-
-    await $`pkill -P ${pids}`; // Termina los pid hijos (cierra también el proceso de node padre)
-    await $`kill -9 ${pids}`; // Termina con el proceso en sí (por si no se ha cerrado solo)
-  }
-} catch (e) {
-  console.error("Error:", e.message);
-}
+  await $`pkill -P ${pids}`; // Termina los pid hijos (cierra también el proceso de node padre)
+  await $`kill -9 ${pids}`; // Termina con el proceso en sí (por si no se ha cerrado solo)
+} else
+  console.log("No processes to stop.");
