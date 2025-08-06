3dc5e364941b25e05f0936f94d2205a2401d26e1
Daniel Sales <danisales.es@gmail.com>
2025-08-06 23:37:27 +0200

    chore: fix script

--- packages/vlc/bin/stop.mjs
+++ packages/vlc/bin/stop.mjs
@@ -8,7 +8,7 @@ const result = await $`ps -eo pid,cmd | grep ${startScript} | grep -v grep |
 const pid = (result.stdout ?? "").trim();
 
 if (pid) {
-  const pids = pid.stdout.trim().replace(/\n/g, " ");
+  const pids = pid.trim().replace(/\n/g, " ");
 
   await $`pkill -P ${pids}`; // Termina los pid hijos (cierra también el proceso de node padre)
   await $`kill -9 ${pids}`; // Termina con el proceso en sí (por si no se ha cerrado solo)
