cad98d8a6fa5eb49fbf68b46a5e9de9705a10bd5
Daniel Sales <danisales.es@gmail.com>
2025-09-17 18:42:17 +0200

    fix(back): reanudable job

--- packages/server/src/modules/musics/admin/update-file-info/task.handler.ts
+++ packages/server/src/modules/musics/admin/update-file-info/task.handler.ts
@@ -1,8 +1,10 @@
+/* eslint-disable no-underscore-dangle */
 import { Injectable, Logger } from "@nestjs/common";
 import { Job } from "bullmq";
 import { TasksCrudDtos } from "$shared/models/tasks";
 import z from "zod";
 import { MusicTasks } from "$shared/models/musics/admin";
+import { MusicFileInfoEntity } from "$shared/models/musics/file-info";
 import { MusicFileInfoRepository } from "#musics/file-info/crud/repository";
 import { TaskHandlerClass, TaskHandler, TaskService } from "#core/tasks";
 import { MusicFileInfoOmitMusicIdBuilder } from "#musics/file-info/builder";
@@ -18,6 +20,17 @@ type Payload = z.infer<typeof payloadSchema>;
 type Result = undefined;
 export type Progress = z.infer<typeof progressSchema>;
 
+type Internal = Partial<{
+  all: MusicFileInfoEntity[];
+  i: number;
+}>;
+
+type JobWithInternal = Job & {
+  data: {
+    _internal: Internal;
+  };
+};
+
 @Injectable()
 @TaskHandlerClass()
 export class MusicUpdateFileInfoTaskHandler implements TaskHandler<Payload, Result> {
@@ -30,21 +43,26 @@ export class MusicUpdateFileInfoTaskHandler implements TaskHandler<Payload, Resu
     private readonly fileInfosRepo: MusicFileInfoRepository,
   ) {}
 
-  async execute(_payload: Payload, job: Job): Promise<Result> {
-    const all = await this.fileInfosRepo.getAll();
-    let i = 0;
+  async execute(_payload: Payload, job: JobWithInternal): Promise<Result> {
+    const all = job.data._internal?.all ?? await this.fileInfosRepo.getAll();
+
+    await this.updateInternal(job, (old)=>( {
+      ...old,
+      all,
+    } ));
 
     await job.updateProgress( {
       percentage: 1,
       message: "Starting ...",
     } );
 
-    for (const fileInfo of all) {
+    for (let i = job.data._internal?.i ?? 0; i < all.length; i++) {
+      const fileInfo = all[i];
       const relativePath = fileInfo.path;
 
       await job.updateProgress( {
         percentage: 1 + ((99 - 1) * (i / all.length)),
-        message: `${++i} / ${all.length}: ${relativePath}`,
+        message: `${i + 1} / ${all.length}: ${relativePath}`,
       } );
       const file = await new MusicFileInfoOmitMusicIdBuilder().withPartial( {
         path: relativePath,
@@ -52,11 +70,25 @@ export class MusicUpdateFileInfoTaskHandler implements TaskHandler<Payload, Resu
         .build();
 
       await this.fileInfosRepo.patchOneByPath(relativePath, file);
+
+      await this.updateInternal(job, (old)=>( {
+        ...old,
+        i,
+      } ));
     }
 
     await job.updateProgress( {
       percentage: 100,
       message: "Done!",
+    } );
+  }
+
+  updateInternal(job: JobWithInternal, fn: (old: Internal)=> Internal) {
+    const _internal: Internal = job.data._internal ? fn(job.data._internal) : fn( {} );
+
+    return job.updateData( {
+      ...job.data,
+      _internal,
     } );
   }
 
