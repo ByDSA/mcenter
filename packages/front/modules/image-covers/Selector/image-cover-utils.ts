31f92fc543e6c0708d74c4d5b9563bd247289b2d
Daniel Sales <danisales.es@gmail.com>
2026-03-13 23:39:09 +0100

    fix: update image realtime when image cover changes

--- packages/front/modules/image-covers/Selector/image-cover-utils.ts
+++ packages/front/modules/image-covers/Selector/image-cover-utils.ts
@@ -1,39 +1,39 @@
 import { PATH_ROUTES } from "$shared/routing";
-import { backendUrl } from "#modules/requests";
 import { ImageCover } from "../models";
+import { backendUrl } from "#modules/requests";
 
 export function getLargeCoverUrl(imageCover: ImageCover): string {
   const fieldUrl = imageCover.versions.large
       ?? imageCover.versions.original;
 
-  return getUrlFromField(fieldUrl);
+  return getUrlFromField(fieldUrl, imageCover.updatedAt);
 }
 
-function getUrlFromField(fieldUrl: string) {
-  if (fieldUrl.includes("/"))
-    return fieldUrl;
+function getUrlFromField(fieldUrl: string, updatedAt: Date) {
+  const t = new Date(updatedAt).getTime();
+  const base = fieldUrl.includes("/")
+    ? fieldUrl
+    : backendUrl(PATH_ROUTES.imageCovers.raw.withParams(fieldUrl));
 
-  return backendUrl(
-    PATH_ROUTES.imageCovers.raw.withParams(fieldUrl),
-  );
+  return `${base}?t=${t}`;
 }
 
 export function getMediumCoverUrl(imageCover: ImageCover): string {
   const fieldUrl = imageCover.versions.medium
     ?? imageCover.versions.large ?? imageCover.versions.original;
 
-  return getUrlFromField(fieldUrl);
+  return getUrlFromField(fieldUrl, imageCover.updatedAt);
 }
 
 export function getSmallCoverUrl(imageCover: ImageCover): string {
   const fieldUrl = imageCover.versions.small
       ?? imageCover.versions.medium ?? imageCover.versions.large ?? imageCover.versions.original;
 
-  return getUrlFromField(fieldUrl);
+  return getUrlFromField(fieldUrl, imageCover.updatedAt);
 }
 
 export function getOriginalCoverUrl(imageCover: ImageCover): string {
   const fieldUrl = imageCover.versions.original;
 
-  return getUrlFromField(fieldUrl);
+  return getUrlFromField(fieldUrl, imageCover.updatedAt);
 }
